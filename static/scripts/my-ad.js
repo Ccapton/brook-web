c14833cc0947b5687845d58df408fb0cd007fbb9
ccapton <chenweibin1125@foxmail.com>
2018-10-31 01:10:00 +0800

    广告、公告功能

--- static/scripts/my-ad.js
+++ static/scripts/my-ad.js
@@ -1,8 +1,21 @@
 
 pin=1125;
 ad_name='brook-web';
-api_path='http://192.168.1.106:5001/api/webad'
+api_path='http://111.230.231.107:5001/api/webad';
+//api_path='http://192.168.1.106:5001/api/webad';
 
 function getAd(){
+    $.get(api_path,{'pin':1125,'ad_name':ad_name},function (result) {
+         if (result.code == 0){
+             $("#notice").append(result.data);
 
+         }else {
+             console.warn('公告信息获取失败')
+         }
+
+     })
 }
+
+$(document).ready(function () {
+     getAd()
+});
